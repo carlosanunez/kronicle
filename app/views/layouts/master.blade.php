39832a5b4ffba3966556aabd9610138d9b077036
Wilfred Denton <dentonw3@gmail.com>
2013-08-06 23:09:14 -0400

    adjusted background gradient for projects

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -31,17 +31,17 @@
             @endif
             @if ($active == 'projects')
                 body {
-                    background-image: linear-gradient(bottom, rgb(78,238,252) 0%, rgb(68,125,250) 55%);
-                    background-image: -o-linear-gradient(bottom, rgb(78,238,252) 0%, rgb(68,125,250) 55%);
-                    background-image: -moz-linear-gradient(bottom, rgb(78,238,252) 0%, rgb(68,125,250) 55%);
-                    background-image: -webkit-linear-gradient(bottom, rgb(78,238,252) 0%, rgb(68,125,250) 55%);
-                    background-image: -ms-linear-gradient(bottom, rgb(78,238,252) 0%, rgb(68,125,250) 55%);
+                    background-image: linear-gradient(bottom, rgb(78,238,252) 36%, rgb(68,125,250) 68%);
+                    background-image: -o-linear-gradient(bottom, rgb(78,238,252) 36%, rgb(68,125,250) 68%);
+                    background-image: -moz-linear-gradient(bottom, rgb(78,238,252) 36%, rgb(68,125,250) 68%);
+                    background-image: -webkit-linear-gradient(bottom, rgb(78,238,252) 36%, rgb(68,125,250) 68%);
+                    background-image: -ms-linear-gradient(bottom, rgb(78,238,252) 36%, rgb(68,125,250) 68%);
                     background-image: -webkit-gradient(
                     linear,
                     left bottom,
                     left top,
-                    color-stop(0, rgb(78,238,252)),
-                    color-stop(0.55, rgb(68,125,250))
+                    color-stop(0.36, rgb(78,238,252)),
+                    color-stop(0.68, rgb(68,125,250))
                     );
                 }
             @endif
